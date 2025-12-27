d8df08d22e41026257f2132da132dced10011da2
Alexander Varga <thealexvarga@gmail.com>
2025-12-27 15:30:11 -0500

    make the example more interesting with some parsing

--- cstree2d/examples/example.rs
+++ cstree2d/examples/example.rs
@@ -1,4 +1,5 @@
 use cstree2d::{cstree::Syntax, green::Builder};
+use indoc::indoc;
 
 /**************************************************************/
 
@@ -11,26 +12,42 @@ pub(crate) enum TestSyntax {
 
 /**************************************************************/
 
-fn main() {
-    let mut builder: Builder<TestSyntax> = Builder::new();
+/// Parse a space-indented block of text.
+fn parse<'a>(builder: &mut Builder<TestSyntax>, s: &'a str) {
     builder.start_node(TestSyntax::Root);
 
-    builder.token(TestSyntax::Text, "def hello_world():");
-    builder.newline();
-    builder.indent("    ");
+    let mut indent = 0;
+    for line in s.lines() {
+        let leading_spaces = line.chars().take_while(|c| *c == ' ').count();
 
-    builder.token(TestSyntax::Text, "print('Hello')");
-    builder.newline();
-
-    builder.token(TestSyntax::Text, "print('World')");
-    builder.newline();
-    builder.dedent();
-
-    builder.token(TestSyntax::Text, "hello_world()");
-    builder.newline();
+        for _ in 0..(leading_spaces.saturating_sub(indent)) {
+            builder.indent(" ");
+        }
+        for _ in 0..(indent.saturating_sub(leading_spaces)) {
+            builder.dedent();
+        }
+        indent = leading_spaces;
+        builder.token(TestSyntax::Text, &line[leading_spaces..]);
+        builder.newline();
+    }
 
     builder.finish_node();
+}
+
+/**************************************************************/
+
+fn main() {
+    let s = indoc! {"
+        def hello_world():
+            print('Hello')
+            print('World')
+        hello_world()
+    "};
+
+    let mut builder: Builder<TestSyntax> = Builder::new();
+    parse(&mut builder, s);
 
     let red = builder.red();
+    println!("{}", red.debug(true));
     println!("{red}");
 }
